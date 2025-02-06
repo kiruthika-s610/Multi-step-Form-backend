4f12abb3a295257f2de0f107e6911e4f4b5ea468
kiruthika-s610 <kiruthika.s610@gmail.com>
2025-02-06 15:17:48 +0530

    delete comment

--- server.js
+++ server.js
@@ -16,39 +16,6 @@ mongoose.connect('mongodb+srv://kiruthikas610:sriadvikS64113@cluster0.bthdi.mong
 .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
     
-
-// const userSchema = new mongoose.Schema({
-//     image:String
-// })
-
-// const usermodel = mongoose.model("user", userSchema)
-
-// const storage = multer.diskStorage({
-//     destination:(req, file, cb)=>{
-//         cb(null, './public/images')
-//     },
-//     filename:(req,file, cb)=>{
-//         cb(null, file.fieldname + '_ ' + Date.now()+path.extname(file.originalname))
-//     }
-// })
-
-// const upload = multer ({
-//        storage:storage
-// })
-
-// app.post('/upload', upload.single('file'), (req,res)=>{
-    
-// usermodel.create({image : req.file.filename}).then(result=>res.json(result)).catch((err)=>console.log(err))
-// })
-
-// app.get('/getimage', (req,res)=>{
-//     usermodel.find().then(user=>res.json(user)).catch(err=>res.json(err))
-// })
-
-
-
-
-
 const formSchema = new mongoose.Schema({
     name: String,
     location: String,
@@ -81,38 +48,7 @@ const formModel = mongoose.model('FormData', formSchema)
 
 const storage = multer.memoryStorage();
 
-// const fileFilter = (req, file, cb) => {
-//     const allowedTypes = /jpeg|jpg|png|pdf/;
-//     const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-//     const mimetype = allowedTypes.test(file.mimetype);
-  
-//     if (extname && mimetype) {
-//       cb(null, true);
-//     } else {
-//       cb(new Error('Only jpg, png, and pdf files are allowed!'));
-//     }
-//   };
-
 const upload = multer({ storage });   
-// const upload = multer({
-//     storage: storage,
-                   // limits: { fileSize: 2 * 1024 * 1024 },
-                   // fileFilter: (req, file, cb) => {
-                   //   if (file.fieldname === "photo") {
-                   //     if (!file.mimetype.match(/^image\/(jpeg|png)$/)) {
-                   //         return cb(new Error("Only JPG and PNG images are allowed for the photo field."));
-                   //       }
-                   //     } else if (file.fieldname === "resume") {
-                   //       // Allow only PDF for resume
-                   //       if (file.mimetype !== "application/pdf") {
-                   //         return cb(new Error("Only PDF files are allowed for the resume field."));
-                   //       }
-                   //     }
-                   //     cb(null, true);
-                   //   },
-    // });
-
-
 
 app.post('/submit-form', upload.fields([
     { name: 'photo', maxCount: 1 },
@@ -121,18 +57,7 @@ app.post('/submit-form', upload.fields([
 try{
     if (!req.files['photo'] || !req.files['resume']) {
         return res.status(400).send({ error: 'Photo or resume file is missing' });
-      }
-
-      // const photoFile = req.files.photo[0];
-      // const resumeFile = req.files.resume[0];
-
-                              // Check file size limits
-                              // if (photoFile.size > 5 * 1024 * 1024) {
-                              //   return res.status(400).send({ error: "Photo size exceeds 5 MB limit." });
-                              // }
-                              // if (resumeFile.size > 15 * 1024 * 1024) {
-                              //   return res.status(400).send({ error: "Resume size exceeds 15 MB limit." });
-                              // }
+      }     
 
     const newForm = new formModel({
     name: req.body.name,
@@ -185,6 +110,10 @@ app.get('/form-data',async (req,res)=>{
         res.status(500).send({ error: 'Failed to retrieve form data' });
       }
     });
+
+    
+
+
 
 app.listen(3000, ()=>{
     console.log("server is listening to the port 3000")
